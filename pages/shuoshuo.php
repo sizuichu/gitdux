84769f4f5eb4dd25ee5684e787dcc16a3ab3fb50
sizuichu <36018846+sizuichu@users.noreply.github.com>
2018-08-15 11:14:28 +0800

    优化说说页面
    优化说说页面

--- pages/shuoshuo.php
+++ pages/shuoshuo.php
@@ -3,42 +3,16 @@ Template Name: 说说
 author: 秋叶
 url: http://www.mizuiren.com/141.html
 */
-
-get_header(); ?>
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-        <div id="shuoshuo_content">
-            <ul class="bsy_timeline">
-                <?php query_posts("post_type=shuoshuo&post_status=publish&posts_per_page=-1");if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <li> <span class="author_tou"><img src="http://www.99bsy.com/authortou.png" class="avatar" width="48" height="48"></span>
-                    <a class="bsy_tmlabel" href="javascript:void(0)">
-                        <div></div>
-                        <div><?php the_content(); ?></div>
-                        <div></div>
-                        <div class="shuoshuo_time"><i class="fa fa-user"></i><?php the_author() ?><i class="fa fa-clock-o"></i><?php the_time('Y年n月j日G:i'); ?>
-                        </div>
-                    </a>
-                    <?php endwhile;endif; ?>
-                </li>
-            </ul>
-        </div>
-    </main>
-    <!-- .site-main -->
+get_header(); ?><div class="shuoshuo">
+ <ul class="archives-monthlisting">
+ <?php query_posts("post_type=shuoshuo&post_status=publish&posts_per_page=-1");if (have_posts()) : while (have_posts()) : the_post(); ?>
+ <li><span class="tt"><?php the_time('Y年n月j日G:i'); ?></span>
+ <div class="shuoshuo-content"><?php the_content(); ?><br/>
+ <div class="shuoshuo-meta"><span >—<?php the_author() ?>
+ <img src="http://127.0.0.1/wordpress/wp-content/themes/gitdux/img/logo.png"style="widtt:3px;height:30px">
+ </span></div></div>
+ <?php endwhile;endif; ?></li>
+ </ul>
 </div>
-<script type="text/javascript">
-    $(function () {
-        var oldClass = "";
-        var Obj = "";
-        $(".bsy_timeline li").hover(function () {
-            Obj = $(this).children(".author_tou");
-            Obj = Obj.children("img");
-            oldClass = Obj.attr("class");
-            var newClass = oldClass + " zhuan";
-            Obj.attr("class", newClass);
-        }, function () {
-            Obj.attr("class", oldClass);
-        })
-    })
-</script>
-<?php get_sidebar(); ?>
+
 <?php get_footer();?>
